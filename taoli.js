24cfb3a90bfb0f2ac845e8dd0f42b6cd65776e2a
Lc613 <3288132604@qq.com>
2025-03-17 16:02:36 +0800

    JAVA

---
+++ taoli.js
@@ -0,0 +1,10 @@
+function sayHello(){
+alert("什么？连自己的名字都不记得了吗？你叫■■■");
+alert("听不清吗？……想重新起一个名字？随便你……");
+var response = prompt("你希望我怎么称呼你？");
+alert("好的"+response+",我想我们终于可以开始了？");
+};
+
+function yifu(){
+alert("太好了！我们马上就可以离开这个房间了！");
+};
